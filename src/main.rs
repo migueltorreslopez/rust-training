26256733c6b40543298a2d563b2f43896dfc43c7
Miguel Torres <migueltorreslopez@gmail.com>
2017-04-26 17:51:58 -0700

    Add int converter using match pattern

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ fn main(){
 	let my_enum = MyEnum::Else;
 
 	my_match(my_enum);
+	print_int(2);
+
 
 	enum MyEnum {
 		MyPrint,
@@ -14,6 +16,15 @@ fn main(){
 		match msg{
 			MyEnum::MyPrint => println!("In My Print!"),
 			_ => println!("In Else")
+		}
+	}
+
+	// This method receives an integer and print a string representation of it
+	fn print_int(my_int: i32) {
+		match my_int {
+			1 => println!("One"),
+			2 => println!("Two"),
+			_ => println!("Not one or two")
 		}
 	}
 
